7f5b03210a6a64fedeb592f9aafa007f9d0fc85b
Yanchao Murong <yanchao.murong@gmail.com>
2023-02-23 00:14:48 +0100

    custom spark images

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -e
 
 TAG=3.3.0-hadoop3.3
+NAMESPACE=ymurong
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=$NAMESPACE/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
@@ -19,12 +20,12 @@ if [ $# -eq 0 ]
     build master
     build worker
     build history-server
-    build submit
-    build maven-template template/maven
-    build sbt-template template/sbt
-    build python-template template/python
+#    build submit
+#    build maven-template template/maven
+#    build sbt-template template/sbt
+#    build python-template template/python
     
-    build python-example examples/python
+#    build python-example examples/python
   else
     build $1 $2
 fi
